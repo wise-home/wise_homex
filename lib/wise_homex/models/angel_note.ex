188727fb6c5e57d9482b724e8f4a49723305c645
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2019-03-19 10:12:17 +0100

    Add AngelNote model

---
+++ lib/wise_homex/models/angel_note.ex
@@ -0,0 +1,12 @@
+defmodule WiseHomex.AngelNote do
+  @moduledoc false
+
+  use WiseHomex.BaseModel
+
+  embedded_schema do
+    field(:target_type, :string)
+    field(:target_id, :string)
+    field(:content, :string)
+    timestamps(type: :utc_datetime)
+  end
+end
