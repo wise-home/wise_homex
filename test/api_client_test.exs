785f576918d9fb62e244d252a35f9fcfccae7347
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2019-03-19 14:20:32 +0100

    Set up config in setup

--- test/api_client_test.exs
+++ test/api_client_test.exs
@@ -5,19 +5,18 @@ defmodule WiseHomex.ApiClientTest do
 
   setup do
     MockServer.start_link()
+    config = WiseHomex.new_config(:api_key, "somekey")
 
-    :ok
+    {:ok, %{config: config}}
   end
 
   describe "get_account_users" do
-    test "it calls get_account_users with the expected includes and filters" do
+    test "it calls get_account_users with the expected includes and filters", %{config: config} do
       MockServer.set(
         :get_account_users,
         %{query: %{"include" => "user", "filter[role]" => "tenant"}},
         {:ok, %WiseHomex.Device{}}
       )
-
-      config = WiseHomex.new_config(:api_key, "somekey")
 
       {:ok, _account_users} = config |> WiseHomex.get_account_users(%{"include" => "user", "filter[role]" => "tenant"})
 
@@ -28,14 +27,12 @@ defmodule WiseHomex.ApiClientTest do
   end
 
   describe "angel notes" do
-    test "it gets angel note" do
+    test "it gets an angel note", %{config: config} do
       MockServer.set(
         :get_angel_note,
         %{target_type: "devices", target_id: "100"},
         {:ok, %WiseHomex.AngelNote{}}
       )
-
-      config = WiseHomex.new_config(:api_key, "somekey")
 
       {:ok, _angel_note} = config |> WiseHomex.get_angel_note("devices", "100")
 
@@ -44,10 +41,9 @@ defmodule WiseHomex.ApiClientTest do
   end
 
   describe "ping" do
-    test "it calls ping with the expected includes" do
+    test "it calls ping with the expected includes", %{config: config} do
       MockServer.set(:ping, %{query: %{"include" => "user,account"}}, {:ok, 1234})
 
-      config = WiseHomex.new_config(:api_key, "somekey")
       {:ok, 1234} = config |> WiseHomex.ping(%{"include" => "user,account"})
       assert MockServer.called?(:ping) == %{query: %{"include" => "user,account"}}
     end
