77d43a95ec8bb35aaef046a0ef90a58693cbf0f5
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2019-05-27 15:41:07 +0200

    Move tenancy functions

--- lib/wise_homex/api_client_impl.ex
+++ lib/wise_homex/api_client_impl.ex
@@ -623,25 +623,7 @@ defmodule WiseHomex.ApiClientImpl do
     Request.delete(config, "/rooms/" <> id)
   end
 
-  def get_wmbus_cache(config, gateway_id, query \\ %{}) do
-    Request.get(config, "/gateways/" <> gateway_id <> "/wmbus-meters/cache", query)
-  end
-
-  def refresh_wmbus_cache(config, gateway_id) do
-    Request.post(config, "/gateways/" <> gateway_id <> "/wmbus-meters/cache")
-  end
-
-  def get_users(config, filters \\ []) do
-    query =
-      filters
-      |> Enum.map(fn {key, value} ->
-        "filter[#{key}]=#{value}"
-      end)
-      |> Enum.join("&")
-
-    query = if query == "", do: query, else: "?#{query}"
-    Request.get(config, "/users#{query}")
-  end
+  # Tenancy
 
   def get_tenancy(config, id, query \\ %{}) do
     Request.get(config, "/tenancies/" <> id, query)
@@ -677,6 +659,26 @@ defmodule WiseHomex.ApiClientImpl do
 
   def delete_tenancy(config, id) do
     Request.delete(config, "/tenancies/" <> id)
+  end
+
+  def get_wmbus_cache(config, gateway_id, query \\ %{}) do
+    Request.get(config, "/gateways/" <> gateway_id <> "/wmbus-meters/cache", query)
+  end
+
+  def refresh_wmbus_cache(config, gateway_id) do
+    Request.post(config, "/gateways/" <> gateway_id <> "/wmbus-meters/cache")
+  end
+
+  def get_users(config, filters \\ []) do
+    query =
+      filters
+      |> Enum.map(fn {key, value} ->
+        "filter[#{key}]=#{value}"
+      end)
+      |> Enum.join("&")
+
+    query = if query == "", do: query, else: "?#{query}"
+    Request.get(config, "/users#{query}")
   end
 
   def get_sims(config, query \\ %{}) do
