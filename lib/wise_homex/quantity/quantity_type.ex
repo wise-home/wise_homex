3b9cd3b6cbb51231a4ea42ed6299861ceacb45da
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2019-09-12 19:54:48 +0200

    Quantity

---
+++ lib/wise_homex/quantity/quantity_type.ex
@@ -0,0 +1,27 @@
+defmodule WiseHomex.Quantity.Type do
+  @moduledoc """
+  An Ecto type for WiseHomex.Quantity
+  """
+
+  use Ecto.Type
+
+  alias WiseHomex.Quantity
+
+  @impl Ecto.Type
+  def type, do: :string
+
+  @impl Ecto.Type
+  def cast(value) when is_binary(value), do: Quantity.parse(value)
+  def cast(%Quantity{} = value), do: {:ok, value}
+  def cast(nil), do: nil
+  def cast(_), do: :error
+
+  @impl Ecto.Type
+  def load(value) when is_binary(value), do: Quantity.parse(value)
+  def load(nil), do: nil
+  def load(_), do: :error
+
+  @impl Ecto.Type
+  def dump(%Quantity{} = value), do: {:ok, Quantity.to_string(value)}
+  def dump(_), do: :error
+end
---
+++ test/quantity/quantity_test.exs
@@ -0,0 +1,33 @@
+defmodule WiseHomex.QuantityTest do
+  use WiseHomex.Test.Case
+
+  alias WiseHomex.Quantity
+
+  doctest Quantity
+
+  test "parsing and encoding" do
+    examples = [
+      ~Q[12.45 DKK],
+      ~Q[65465465465465464.64654654654654654 kw/h],
+      ~Q[0 units],
+      ~Q[5 super powers],
+      ~Q[42E1 wh]
+    ]
+
+    Enum.each(examples, fn quantity ->
+      encoded = Quantity.to_string(quantity)
+      {:ok, parsed} = Quantity.parse(encoded)
+
+      assert is_binary(encoded)
+      assert parsed == quantity
+    end)
+  end
+
+  test "inspecting" do
+    assert ~Q[12.45 DKK] |> inspect() == "~Q[12.45 DKK]"
+  end
+
+  test "text interpolating" do
+    assert "#{~Q[12.45 DKK]}" == "12.45 DKK"
+  end
+end
