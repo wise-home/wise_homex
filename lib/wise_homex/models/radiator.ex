bdebf3efb0e305504c4db3005e2e3b91280c5d04
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2019-12-18 09:14:58 +0100

    Sort raditator fields, add :info string field

--- lib/wise_homex/models/radiator.ex
+++ lib/wise_homex/models/radiator.ex
@@ -8,20 +8,21 @@ defmodule WiseHomex.Radiator do
   embedded_schema do
     has_many :heat_sources, WiseHomex.HeatSource
 
-    field :internal_id, :integer
-    field :group, :string
-    field :group_id, :integer
-    field :manufacturer, :string
-    field :model, :string
-    field :illustration_number, :integer
-    field :surface_type, :string
-    field :radiator_type, :string
+    field :alt_height, :integer
+    field :archived_at, :utc_datetime
     field :columns, :integer
     field :depth, :integer
-    field :height, :integer
-    field :alt_height, :integer
     field :element_length, :integer
+    field :group, :string
+    field :group_id, :integer
+    field :height, :integer
+    field :info, :string
+    field :illustration_number, :integer
+    field :internal_id, :integer
+    field :manufacturer, :string
+    field :model, :string
     field :performance_per_element, :float
-    field :archived_at, :utc_datetime
+    field :radiator_type, :string
+    field :surface_type, :string
   end
 end
