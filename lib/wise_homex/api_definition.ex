df528df562e112f95d97888cd29dfe6d2c316102
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-01-06 09:05:40 +0100

    Refactor to use standalone api_config.exs as the definition of automatically generated endpoints

--- lib/wise_homex/api_definition.ex
+++ lib/wise_homex/api_definition.ex
@@ -3,75 +3,74 @@ defmodule WiseHomex.ApiDefinition do
   The definition of the Wise Home API
   """
 
-  defmodule APIResource do
+  defmodule Reader do
     @moduledoc """
-    A definition of an api resource
+    Helper module for reading the API configuration.
+    Separating the modules this way allows us to use the function inside a module attribute.
     """
 
-    @type t :: %__MODULE__{
-            name_singular: String.t(),
+    @typedoc """
+    Supported endpoint types
+    """
+    @type verb :: :index | :show | :create | :update | :delete
+
+    @typedoc """
+    Definition of multiple api endpoints as given in automated_api_definition.exs
+    """
+    @type resource_definition :: %{
+            endpoints: [atom],
             name_plural: String.t(),
+            name_singular: String.t(),
             path: String.t(),
             type: String.t()
           }
 
-    defstruct [:name_singular, :name_plural, :path, :type]
-  end
+    @typedoc """
+    Definition of a single endpoint
+    """
+    @type single_endpoint :: %{
+            name_plural: String.t(),
+            name_singular: String.t(),
+            path: String.t(),
+            type: String.t()
+          }
 
-  @type endpoint_name :: atom()
+    def api_resources_from_file(filename) do
+      # Handle when called directly from Code.eval_file/1, which returns a 2-tuple
+      {_, variables} = filename |> Code.eval_file()
+      variables |> Keyword.fetch!(:endpoints) |> to_api_resources()
+    end
 
-  @type endpoint_definition :: %{endpoints: [atom], path: String.t(), type: String.t()}
-
-  @type api_definition :: %{endpoint_name => endpoint_definition}
-
-  @doc """
-  The Wise Home API as a list of ApiResource structs
-  """
-  @spec api_resources() :: [APIResource.t()]
-  def api_resources() do
-    api_definition() |> to_api_resources()
-  end
-
-  # The definition of the Wise Home API
-  defp api_definition() do
-    %{
-      expense: %{
-        endpoints: [:index, :show, :create, :update, :delete],
-        path: "/expenses",
-        type: "expenses"
-      }
-    }
-  end
-
-  # Transforms the api definition into an APIResource struct representing every function that should be created
-  defp to_api_resources(api_definition) do
-    api_definition
-    |> Enum.flat_map(fn {resource_name, %{endpoints: endpoints, path: path, type: type}} ->
+    @doc """
+    Transform the api resource definition into a list of single api functions
+    """
+    @spec to_api_resources([resource_definition]) :: [{verb, single_endpoint}]
+    def to_api_resources(endpoints) do
       endpoints
-      |> Enum.map(fn verb ->
-        api_resource = %APIResource{
-          name_singular: resource_name |> Atom.to_string(),
-          name_plural: type |> snake_case(),
-          path: path,
-          type: type
-        }
+      |> Enum.flat_map(fn %{endpoints: endpoints} = resource ->
+        endpoints
+        |> Enum.map(fn verb ->
+          api_resource = %{
+            name_singular: resource.name_singular,
+            name_plural: resource.name_plural,
+            path: resource.path,
+            type: resource.type
+          }
 
-        {verb, api_resource}
+          {verb, api_resource}
+        end)
       end)
-    end)
+    end
   end
 
+  @external_resource "lib/wise_homex/api_config.exs"
+  @api_resources "api_config.exs"
+                 |> Path.expand("./lib/wise_homex")
+                 |> Reader.api_resources_from_file()
+
   @doc """
-  Transform a kebab cased string to snake case
-
-  iex> ApiDefinition.snake_case("large-households")
-  "large_households"
-
-  iex> ApiDefinition.snake_case("households")
-  "households"
-
-  iex> ApiDefinition.snake_case("")
-  ""
+  The Wise Home API as a list of single functions to be implemented including verbs
   """
-  def snake_case(input), do: input |> String.replace("-", "_")
+  @spec api_resources() :: [{Reader.verb(), Reader.single_endpoint()}]
+  def api_resources(), do: @api_resources
 end
