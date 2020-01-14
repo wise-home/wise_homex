04af926bfe575bb164ff971b03d19bb06cf2d707
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-01-14 14:35:16 +0100

    Add account_payment relation to fiscal_year.ex

--- lib/wise_homex/models/fiscal_year.ex
+++ lib/wise_homex/models/fiscal_year.ex
@@ -9,6 +9,7 @@ defmodule WiseHomex.FiscalYear do
     belongs_to(:property, WiseHomex.Property)
     belongs_to(:statement, WiseHomex.Statement)
     has_many(:expenses, WiseHomex.FiscalYear)
+    has_many(:account_payments, WiseHomex.AccountPayment)
 
     field(:utility_type, :string)
     field(:start_date, :date)
