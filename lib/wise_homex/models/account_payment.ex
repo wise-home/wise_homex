9dca0d7c25e9412c0d97110cf607c337b9a818d0
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2020-04-02 15:01:04 +0200

    Add excluded on AccountPayment

--- lib/wise_homex/models/account_payment.ex
+++ lib/wise_homex/models/account_payment.ex
@@ -8,10 +8,11 @@ defmodule WiseHomex.AccountPayment do
   embedded_schema do
     belongs_to :tenancy, WiseHomex.Tenancy, type: :binary_id
     belongs_to :fiscal_year, WiseHomex.FiscalYear, type: :binary_id
+    field :accounting_date, :date
+    field :currency, :string
+    field :excluded, :boolean
     field :total_amount, :integer
     field :vat_amount, :integer
-    field :currency, :string
-    field :accounting_date, :date
 
     timestamps type: :utc_datetime
   end
