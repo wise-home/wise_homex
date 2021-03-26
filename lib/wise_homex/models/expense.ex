46c98ae6bdadeb6a2424788b4ff7847239295dfb
Niels Kristian Hansen Skovmand <niels@wisehome.dk>
2021-03-26 10:50:49 +0100

    Use quantity fields on Expense

--- lib/wise_homex/models/expense.ex
+++ lib/wise_homex/models/expense.ex
@@ -11,18 +11,11 @@ defmodule WiseHomex.Expense do
     field :accounting_date, :date
     field :company_name, :string
     field :currency, :string
-    field :exponent, :integer
     field :fuel_type, :string
-    field :quantity, :integer
-    field :total_amount, :integer
-    field :unit, :string
-    field :vat_amount, :integer
     field :partial, :boolean
-
-    # Tempary variables during refactoring.
-    field :q_total_amount, WiseHomex.QuantityType
-    field :q_vat_amount, WiseHomex.QuantityType
-    field :q_usage, WiseHomex.QuantityType
+    field :total_amount, WiseHomex.QuantityType
+    field :usage, WiseHomex.QuantityType
+    field :vat_amount, WiseHomex.QuantityType
 
     timestamps type: :utc_datetime
   end
