44639a01f5df1c42bcedb26a1242878be1dbd309
Niklas Johansson <raphexion@gmail.com>
2021-07-06 12:55:18 +0200

    models: Add ReadyInstallationMatch

---
+++ lib/wise_homex/models/ready_installation_match.ex
@@ -0,0 +1,15 @@
+defmodule WiseHomex.ReadyInstallationMatch do
+  @moduledoc false
+
+  use WiseHomex.BaseModel
+
+  alias WiseHomex.Household
+  alias WiseHomex.ReadyInstallation
+
+  embedded_schema do
+    belongs_to :household, Household
+    belongs_to :ready_installation, ReadyInstallation, type: :binary_id
+
+    field :distance, :integer
+  end
+end
