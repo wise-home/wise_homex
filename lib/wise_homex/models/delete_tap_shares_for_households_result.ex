48fa263db29812e42e83aed858913406e259ffb2
Sebastian Bucki <sebu@wisehome.dk>
2023-11-21 09:13:00 +0100

    Remove id from result

--- lib/wise_homex/models/delete_tap_shares_for_households_result.ex
+++ lib/wise_homex/models/delete_tap_shares_for_households_result.ex
@@ -5,6 +5,8 @@ defmodule WiseHomex.DeleteTapSharesForHouseholdsResult do
 
   use WiseHomex.BaseModel
 
+  @primary_key false
+
   embedded_schema do
     field :household_ids_deleted_tap_shares, {:array, :integer}
     field :household_ids_limited_active_period_tap_shares, {:array, :integer}
